81e7eab2788abb224d94ed703d33e68b2a8679ff
Maximilian Jakob Maag <maagmaximilian@yourlocalcloud.de>
2026-03-18 14:49:59 +0000

    save

--- shell.nix
+++ shell.nix
@@ -3,9 +3,13 @@
 pkgs.mkShell {
   buildInputs = [
     pkgs.terraform
+    pkgs.texliveFull
+    pkgs.gnumake
   ];
 
   shellHook = ''
     echo "Terraform CLI ready: $(terraform version)"
+    echo "TeX Live ready: $(pdflatex --version | head -n1)"
+    echo "Make ready: $(make --version | head -n1)"
   '';
 }
